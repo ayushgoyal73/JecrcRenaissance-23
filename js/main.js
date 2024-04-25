40f83934f370a3ffcb75381abe29d5ec70e1fc4f
harshit03singhal <singhalharshit2003@gmail.com>
2024-04-25 23:31:41 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+drawer = document.getElementById("drawer");
+overlay = document.getElementById("mobile-body-overly");
+document
+    .getElementById("drawer-toggler")
+    .addEventListener("click", function () {
+        if (drawer.attributes.expanded.value == "false") {
+            drawer.classList.remove("drawer-inactive");
+            overlay.style = "display:block";
+            drawer.classList.add("drawer-active");
+            drawer.attributes.expanded.value = "true";
+        }
+    });
+
+document
+    .getElementById("drawer-close-btn")
+    .addEventListener("click", function () {
+        if (drawer.attributes.expanded.value == "true") {
+            drawer.classList.add("drawer-inactive");
+            overlay.style = "display:none";
+            drawer.classList.remove("drawer-active");
+            drawer.attributes.expanded.value = "false";
+        }
+    });
+document
+    .getElementById("mobile-body-overly")
+    .addEventListener("click", function () {
+        if (drawer.attributes.expanded.value == "true") {
+            drawer.classList.add("drawer-inactive");
+            overlay.style = "display:none";
+            drawer.classList.remove("drawer-active");
+            drawer.attributes.expanded.value = "false";
+        }
+    });
+$("#drawer .nav-link").click(function () {
+        if (drawer.attributes.expanded.value == "true") {
+            drawer.classList.add("drawer-inactive");
+            overlay.style = "display:none";
+            drawer.classList.remove("drawer-active");
+            drawer.attributes.expanded.value = "false";
+        }
+});
+    
